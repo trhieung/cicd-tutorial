2c9ec3b03a89c7d3251494af0c9534a72b0ae0f3
trhieung <nthieu.ngtr@gmail.com>
2024-07-15 17:39:43 +0700

    setting only postgres

--- services/server/server/settings.py
+++ services/server/server/settings.py
@@ -114,22 +114,22 @@ DATABASES_ALL = {
     #     "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
     #     "PORT": int(os.environ.get("POSTGRES_PORT", "5432")),
     # },
-    DB_POSTGRESQL: {
-        "ENGINE": "django.db.backends.postgresql",
-        "HOST": os.environ.get("POSTGRES_HOST", "0.0.0.0"),
-        "NAME": os.environ.get("POSTGRES_NAME", "postgres"),
-        "USER": os.environ.get("POSTGRES_USER", "postgres"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
-        "PORT": int(os.environ.get("POSTGRES_PORT", "5432")),
-    },
     # DB_POSTGRESQL: {
     #     "ENGINE": "django.db.backends.postgresql",
-    #     "HOST": os.environ.get("POSTGRES_HOST"),
-    #     "NAME": os.environ.get("POSTGRES_NAME"),
-    #     "USER": os.environ.get("POSTGRES_USER"),
-    #     "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-    #     "PORT": int(os.environ.get("POSTGRES_PORT")),
+    #     "HOST": os.environ.get("POSTGRES_HOST", "0.0.0.0"),
+    #     "NAME": os.environ.get("POSTGRES_NAME", "postgres"),
+    #     "USER": os.environ.get("POSTGRES_USER", "postgres"),
+    #     "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
+    #     "PORT": int(os.environ.get("POSTGRES_PORT", "5432")),
     # },
+    DB_POSTGRESQL: {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "NAME": os.environ.get("POSTGRES_NAME"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "PORT": int(os.environ.get("POSTGRES_PORT")),
+    },
 }
 
 ## Using Postgres
